0c9b0ec880c75c32c8fc66606abb77f95e6fcc44
linlingaixuexi <78462907+linlingaixuexi@users.noreply.github.com>
2022-11-15 21:19:57 -0800

    modify index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,14 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+var routes = require("./routes.js");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 /* ---- (Geographical distribution of the companies) ---- */
 app.get("/home/distribution", routes.getCompDistribution);
 
