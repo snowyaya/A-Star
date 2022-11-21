66a8a44647fb7fe5dde30c064866670134608fa4
snowyaya <yayingliang@gmail.com>
2022-11-20 23:52:05 -0500

    Refactor to show homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,25 @@
 import React from "react";
 import FundingPage from './pages/FundingPage';
+import HomePage from './pages/HomePage';
 import ReactDOM from 'react-dom'; 
 
 import {
+  BrowserRouter,
 	BrowserRouter as Router,
 	Route,
-	Switch
+	Routes
 } from 'react-router-dom';
 
 class App extends React.Component {
-  // state = {
-  //   name: ""
-  // }
-
-  // componentDidMount() {
-  //   fetch("http://localhost:3000")
-  //     .then(res => res.json())
-  //     .then(data => this.setState({ name: data.name }))
-  // }
-
   render() {
-    // return (
-    //   <h1>Hello {this.state.name}!</h1>
-    // )
-    return <FundingPage />
-    // return (
-    //   <div>
-    //     <Router>
-    //       <Switch>
-    //         <Route exact
-    //             path="/"
-    //             render={() => (
-    //               <FundingPage />
-    //             )}/>   
-    //       </Switch> 
-    //     </Router>
-
-    //   </div>
-    // )
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/funding" element={<FundingPage />} />
+        </Routes>
+      </BrowserRouter>
+    )
   }
 }
 
