a19d623b9a5085a4c485355c644295585a22e7e9
snowyaya <yayingliang@gmail.com>
2022-12-15 10:23:35 -0500

    Update UI of the form in signup page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,28 +1,95 @@
-import PageNavBar from "./PageNavbar";
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+import PageNavBar from './PageNavbar';
 
-function SignUp() {
+const Signup = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <div>
-        <PageNavBar />
-      <h1>SignUp</h1>
-      <form>
-        <label>
-            Name:
-            <input type="text" name="name" />
-        </label>
-        <label>
-            Email:
-            <input type="text" name="email" />
-        </label>
-        <label>
-            Password:
-            <input type="text" name="password" />
-        </label>
-        <input type="submit" value="Submit" />
-        </form>
+      <PageNavBar active="signup" />
+      <div >
+        <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 6,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit" style={{
+              alignContent: "left",
+              color: "white",
+              backgroundColor: "#acb6e5",
+              fontWeight: "bold",
+            }}>
+            Submit
+          </Button>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit" style={{
+              alignContent: "left",
+              color: "white",
+              backgroundColor: "gray",
+              fontWeight: "bold",
+            }}>
+            Cancel
+          </Button>
+        </Form.Item>
+
+      </Form>
+      </div>
     </div>
+    
   );
-}
-
-export default SignUp;
+};
+export default Signup;
